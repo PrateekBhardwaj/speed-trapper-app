1e7b994fd2da89ee5f44e30ce16f8b77ba3069b2
PrateekBhardwaj <prateekbhardwaj.02@gmail.com>
2014-11-24 17:08:20 +0530

    Create SpeedTrapController.cs

---
+++ SpeedTrapController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_Exzeo.Controllers
+{
+    public class SpeedtrapController : Controller
+    {
+        //
+        // GET: /Speedtrap/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
