108b409c5f8d44600bb6445b3d3f9ce4b38050e0
PrateekBhardwaj <prateekbhardwaj.02@gmail.com>
2014-11-24 17:07:35 +0530

    Create CarsController.cs

---
+++ CarsController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_Exzeo.Controllers
+{
+
+    public class CarsController : Controller
+    {
+        //
+        // GET: /Cars/
+
+        public ActionResult Index()
+        {
+            Random r1 = new Random();
+            ViewBag.Speed = r1.Next(10, 200);
+
+            string[] carNames = System.IO.File.ReadAllLines(@"C:\Users\Bhavna\Downloads\speed-trapper-app-master\resources\car-names.txt");
+            Random r2 = new Random();
+            ViewBag.Car = carNames[r2.Next(0, 45)];
+            ViewBag.Tag = "current speed";
+            return View();
+        }
+
+    }
+}
